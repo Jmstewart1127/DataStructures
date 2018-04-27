011af44745e1805a1186eeec78660a619d34afd7
Jmstewart1127 <Jmstewart1127@gmail.com>
2018-04-26 21:45:34 -0500

    word count

--- DataStructures/src/exam/WordCount.java
+++ DataStructures/src/exam/WordCount.java
@@ -9,10 +9,10 @@ public class WordCount {
         for (int i=0; i<words.length; i++) {
             // if the word exist, then increase the count by 1
             if (wTable.get(words[i]) != null)
-                wTable.put(words[i], i+1);
+                wTable.put(words[i], wTable.get(words[i])+1);
             // if not, add a new item (word, 1)
             else
-                wTable.put(words[i], i);
+                wTable.put(words[i], 1);
         }
         // print the table
         wTable.forEach((k,v)->System.out.println(k+": "+v));
